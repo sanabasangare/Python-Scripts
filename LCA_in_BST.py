63501fc024048c1d516e9381c1a0797c7bb99daa
Sanaba Sangare <sanabasangare@users.noreply.github.com>
2017-02-16 17:19:45 -0500

    fix: LCA in BST constructor

---
+++ LCA_in_BST.py
@@ -0,0 +1,17 @@
+#  for LCA in BST
+
+class Node:
+    # The constructor to create a new binary search tree node
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+	#  T = tree
+	#  r = root
+	#  n1 = node - left
+	#  n2 = node - right
+
+
+#def B_S_T(T, r, n1, n2):
+	
