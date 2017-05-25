91c412daeed15c610a0dc8fe4247d9e645d7deb8
Sanaba Sangare <sanabasangare@users.noreply.github.com>
2017-05-25 00:08:25 -0400

    fix: add Pep8 style

--- crypto.py
+++ crypto.py
@@ -1,15 +1,16 @@
 """
 Cryptopy:
-This program is used to encrypt words and present 
+This program is used to encrypt words and present
 the encoded message. It can also decrypt a ciphertext
 into plain text
 """
 
+
 def cryptopy():
-  	# hint: reply with "encrypt" or "decrypt"
+    # hint: reply with "encrypt" or "decrypt"
     reply = raw_input("Do you want to encrypt or decrypt.\
  (please type <<encrypt>> or <<decrypt>>")
-    
+
     # If the answer is "encrypt", encode the message.
     if reply == "encrypt":
         return encrypt()
