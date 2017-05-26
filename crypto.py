ca20edab32b60281a14387d1791c1d7f3a79e1ee
Sanaba Sangare <sanabasangare@users.noreply.github.com>
2017-05-26 01:59:09 -0400

    chore: add encryption function

--- crypto.py
+++ crypto.py
@@ -8,14 +8,25 @@ into plain text
 
 def cryptopy():
     # hint: reply with "encrypt" or "decrypt"
-    reply = raw_input("Do you want to encrypt or decrypt.\
- (please type <<encrypt>> or <<decrypt>>")
+    reply = raw_input("Would you like to encrypt or decrypt\
+ (please type 'encrypt' or 'decrypt'): ")
 
     # If the answer is "encrypt", encode the message.
     if reply == "encrypt":
-        return encrypt()
+        return encryption()
     # Otherwise, decode the message.
     elif reply == "decrypt":
-        return decrypt()
+        return decryption()
+
+
+# The encryption function
+def encryption():
+    message = raw_input("Please enter the text you wish to encrypt:")
+    
+    # return the length of the original text
+    length = len(message)
+    start = 0
+    c = 0
+
 
 print cryptopy()
